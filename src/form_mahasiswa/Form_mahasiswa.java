ba35bed926d0619a9a798957afa21e1329016d88
Embun Duta Laksmana <82716426+EmbunDutaLaksmana@users.noreply.github.com>
2021-06-23 07:45:13 +0700

    Master

---
+++ src/form_mahasiswa/Form_mahasiswa.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package form_mahasiswa;
+
+/**
+ *
+ * @author Embun Duta Laksmana
+ */
+public class Form_mahasiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
